bcb612f9f82831ff88540db79451b1b7a2ab68dc
Giovanna Zanetti Borges <94799644+GiihZanetti17@users.noreply.github.com>
2022-10-04 21:42:28 -0300

    Create 10.4.2022.py

---
+++ Home Work/10.4.2022.py
@@ -0,0 +1,31 @@
+#Implemente uma lista duplamente encadeada circular que permita: 
+
+#- Inserir item primeira posição; 
+
+#- Inserir item (última posição); 
+
+#- Inserir item em determinada posição; 
+
+#- Buscar item de determinada posição;  
+
+#- Buscar item por elemento; 
+
+#- Remover item da primeira posição; 
+
+#- Remover item da última posição; 
+
+#- Remover item de determinada posição; 
+
+#- Remover determinado elemento; 
+
+#- Exibir a quantidade de elementos na lista; 
+
+#- Exibir os elementos da lista.
+
+Class no:
+   def_init_(self,valor):
+      self.esquerda = none
+      self.direita = none
+      self.valor = valor
+      
+      
